30310301e63d253c7307aff643777d717f272261
EslamKamel89 <elsamkamelforex@gmail.com>
2025-07-25 23:01:20 +0300

    Implement CarModel model with migration and fillable attributes

--- app/Models/CarModel.php
+++ app/Models/CarModel.php
@@ -5,8 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class CarModel extends Model
-{
+class CarModel extends Model {
     /** @use HasFactory<\Database\Factories\CarModelFactory> */
     use HasFactory;
+    protected $fillable = [
+        'brand_id',
+        'name_ar',
+        'name_en',
+        'year_range',
+    ];
 }
