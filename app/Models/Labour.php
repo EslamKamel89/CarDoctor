be4720cbabc7ad428654f3a50c2549df6364ddfc
EslamKamel89 <elsamkamelforex@gmail.com>
2025-07-27 11:23:05 +0300

    feat(models): implement Labor model - Added migration with fillable properties - Configured necessary casting

--- app/Models/Labour.php
+++ app/Models/Labour.php
@@ -5,8 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Labour extends Model
-{
+class Labour extends Model {
     /** @use HasFactory<\Database\Factories\LabourFactory> */
     use HasFactory;
+    protected $fillable = [
+        'name_ar',
+        'name_en',
+        'base_fee',
+        'is_active',
+    ];
+    protected function casts(): array {
+        return [
+            'is_active' => 'boolean',
+        ];
+    }
 }
--- database/migrations/2025_07_25_194720_create_labours_table.php
+++ database/migrations/2025_07_25_194720_create_labours_table.php
@@ -4,15 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('labours', function (Blueprint $table) {
             $table->id();
+            $table->string('name_ar')->nullable();
+            $table->string('name_en')->nullable();
+            $table->decimal('base_fee', 10, 2)->nullable();
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
     }
@@ -20,8 +22,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('labours');
     }
 };
