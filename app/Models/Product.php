0b7b7d6771e2a09b31bbe25833c31fbb22e5eeb2
EslamKamel89 <elsamkamelforex@gmail.com>
2025-07-27 11:07:23 +0300

    feat(models): implement Product model - Added migration with fillable properties - Configured necessary casting

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,8 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
-{
+class Product extends Model {
     /** @use HasFactory<\Database\Factories\ProductFactory> */
     use HasFactory;
+    protected $fillable = [
+        'category_id',
+        'code',
+        'name_ar',
+        'name_en',
+        'description_en',
+        'description_ar',
+        'buy_price',
+        'sell_price',
+        'min_stock_quantity',
+        'notes',
+    ];
 }
--- database/migrations/2025_07_25_194717_create_products_table.php
+++ database/migrations/2025_07_25_194717_create_products_table.php
@@ -4,15 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id')->nullable()->constrained()->nullOnDelete();
+            $table->string('code')->nullable()->unique()->index();
+            $table->string('name_ar')->nullable()->unique()->index();
+            $table->string('name_en')->nullable()->unique()->index();
+            $table->text('description_en')->nullable();
+            $table->text('description_ar')->nullable();
+            $table->decimal('buy_price', 15, 2)->nullable();
+            $table->decimal('sell_price', 15, 2)->nullable();
+            $table->integer('min_stock_quantity')->default(0);
+            $table->text('notes')->nullable();
             $table->timestamps();
         });
     }
@@ -20,8 +28,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('products');
     }
 };
