bb04430d14f685caae7d3b6b20c5b74ae6f04e99
EslamKamel89 <elsamkamelforex@gmail.com>
2025-07-27 10:50:03 +0300

    feat(models): implement Client model - Added migration with name, email, phone columns - Configured fillable fields and type casts

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -5,8 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Client extends Model
-{
+class Client extends Model {
     /** @use HasFactory<\Database\Factories\ClientFactory> */
     use HasFactory;
+    protected $fillable = [
+        'name_ar',
+        'name_en',
+        'mobile',
+        'email',
+        'address_ar',
+        'address_en',
+        'notes',
+    ];
 }
--- database/migrations/2025_07_25_194716_create_clients_table.php
+++ database/migrations/2025_07_25_194716_create_clients_table.php
@@ -4,24 +4,32 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
+            $table->string('name_ar')->nullable();
+            $table->string('name_en')->nullable();
+            $table->string('mobile')->nullable()->unique();
+            $table->string('email')->nullable()->unique();
+            $table->text('address_ar')->nullable();
+            $table->text('address_en')->nullable();
+            $table->text('notes')->nullable();
             $table->timestamps();
+            $table->index('name_ar');
+            $table->index('name_en');
+            $table->index('mobile');
+            $table->index('email');
         });
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('clients');
     }
 };
