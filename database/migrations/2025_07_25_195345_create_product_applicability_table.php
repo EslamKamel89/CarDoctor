ce9711f8ad8923dbe09c90543a3e9529ba5b509e
EslamKamel89 <elsamkamelforex@gmail.com>
2025-07-27 11:16:08 +0300

    feat(migration): implement product_applicability pivot - Added pivot table migration

--- database/migrations/2025_07_25_195345_create_product_applicability_table.php
+++ database/migrations/2025_07_25_195345_create_product_applicability_table.php
@@ -4,24 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('product_applicability', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('product_id')->constrained('products')->cascadeOnDelete();
+            $table->foreignId('car_model_id')->constrained('car_models')->cascadeOnDelete();
+            $table->text('notes')->nullable();
             $table->timestamps();
+            $table->unique(['product_id', 'car_model_id']);
         });
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('product_applicability');
     }
 };
