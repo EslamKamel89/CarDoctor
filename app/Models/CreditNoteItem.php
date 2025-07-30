e2220f51a601bd641ba844ece4045c8e9f92510e
EslamKamel89 <elsamkamelforex@gmail.com>
2025-07-31 00:36:03 +0300

    feat(db): implement CreditNoteItem migration and fillable property in the model

--- app/Models/CreditNoteItem.php
+++ app/Models/CreditNoteItem.php
@@ -5,8 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class CreditNoteItem extends Model
-{
+class CreditNoteItem extends Model {
     /** @use HasFactory<\Database\Factories\CreditNoteItemFactory> */
     use HasFactory;
+    protected $fillable = [
+        'credit_note_id',
+        'invoice_item_id',
+        'quantity',
+        'refunded_amount',
+    ];
 }
--- database/migrations/2025_07_30_194758_create_credit_note_items_table.php
+++ database/migrations/2025_07_30_194758_create_credit_note_items_table.php
@@ -4,15 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('credit_note_items', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('credit_note_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('invoice_item_id')->constrained('invoice_items')->cascadeOnDelete();
+            $table->integer('quantity');
+            $table->decimal('refunded_amount', 15, 2);
             $table->timestamps();
         });
     }
@@ -20,8 +22,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('credit_note_items');
     }
 };
