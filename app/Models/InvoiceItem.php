f167e806832e199de3dcca5912775eb30d561e2e
EslamKamel89 <elsamkamelforex@gmail.com>
2025-07-30 23:35:24 +0300

    feat(db): implement invoiceItem migration and fillable property in the model

--- app/Models/InvoiceItem.php
+++ app/Models/InvoiceItem.php
@@ -5,8 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class InvoiceItem extends Model
-{
+class InvoiceItem extends Model {
     /** @use HasFactory<\Database\Factories\InvoiceItemFactory> */
     use HasFactory;
+    protected $fillable = [
+        'invoice_id',
+        'product_id',
+        'quantity',
+        'unit_price',
+        'total_price',
+    ];
 }
--- database/migrations/2025_07_30_194725_create_invoice_items_table.php
+++ database/migrations/2025_07_30_194725_create_invoice_items_table.php
@@ -4,15 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('invoice_items', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('invoice_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('product_id')->constrained()->cascadeOnDelete();
+            $table->integer('quantity')->default(1);
+            $table->decimal('unit_price', 15, 2);
+            $table->decimal('total_price', 15, 2);
             $table->timestamps();
         });
     }
@@ -20,8 +23,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('invoice_items');
     }
 };
