38b79d2bc4a30d3c9daf110fee19dda9fca618d4
EslamKamel89 <elsamkamelforex@gmail.com>
2025-07-31 00:33:29 +0300

    feat(db): implement CreditNote migration and fillable property in the model

--- app/Models/CreditNote.php
+++ app/Models/CreditNote.php
@@ -5,8 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class CreditNote extends Model
-{
+class CreditNote extends Model {
     /** @use HasFactory<\Database\Factories\CreditNoteFactory> */
     use HasFactory;
+    protected $fillable = [
+        'invoice_id',
+        'client_id',
+        'credit_note_number',
+        'issue_date',
+        'reason_ar',
+        'reason_en',
+        'total_refund_amount',
+        'payment_refund_method',
+    ];
 }
--- database/migrations/2025_07_30_194743_create_credit_notes_table.php
+++ database/migrations/2025_07_30_194743_create_credit_notes_table.php
@@ -4,15 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('credit_notes', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('invoice_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('client_id')->nullable()->constrained()->nullOnDelete();
+            $table->string('credit_note_number')->unique();
+            $table->date('issue_date');
+            $table->text('reason_ar');
+            $table->text('reason_en');
+            $table->decimal('total_refund_amount', 15, 2);
+            $table->enum('payment_refund_method', ['cash', 'card', 'no-refund'])->unique();
             $table->timestamps();
         });
     }
@@ -20,8 +26,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('credit_notes');
     }
 };
